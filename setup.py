e78bed06fb0105963de32be2c1405009fb770686
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2022-08-14 18:20:53 -0500

    Bump version: 1.2.3 → 1.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="pptrees",
-    version="1.2.3",
+    version="1.2.4",
     description="Parallel Prefix tree generation library",
     url="https://github.com/tdene/synth_opt_adders",
     author="tdene",
